94d6adad7ecff7a6abad2b118423e00c34eb4e1e
zlumsd <Dominc.Lumsden@bbcag.ch>
2022-05-03 12:55:34 +0200

    slideshow and buttons added

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -24,3 +24,37 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+//function to show starting image in slideshow
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+//function to slide through slideshow
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+
+    var i2;
+    var x2 = document.getElementsByClassName("imageSliderName");
+    if (n > x2.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x2.length
+    }
+    for (i2 = 0; i2 < x2.length; i2++) {
+        x2[i2].style.display = "none";
+    }
+    x2[slideIndex - 1].style.display = "block";
+}
